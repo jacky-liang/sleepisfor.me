80ae87b3b88fc10d54a2f3d236bc40e72ebda364
Jacky Liang <jackyliang@berkeley.edu>
2015-11-21 13:27:19 -0800

    wip

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('#particle-bg').particleground({
+       density: 7000,
+       particleRadius: 5,
+       maxSpeedX: 0.5,
+       maxSpeedY: 0.5,
+       dotColor: '#b0d6f0',
+       lineColor: '#004471'
+   }); 
+})
