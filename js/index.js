8fb4394ea6aba7c4420332a59c458f260c0e2fe2
Jacky Liang <jackyliang@berkeley.edu>
2015-11-21 20:20:42 -0800

    finished input

--- js/index.js
+++ js/index.js
@@ -8,8 +8,47 @@ $(document).ready(function(){
        lineColor: '#004471'
    }); 
    
-   $('#t_us').timepicker();
-   $('#t_ut').timepicker();
-   $('#t_rs').timepicker();
-   $('#t_rt').timepicker();
+   var t_us = $('#t_us'),
+         t_ut = $('#t_ut'),
+         t_rs = $('#t_rs'),
+         t_rt = $('#t_rt');
+   
+   //MSM is minutes since midnight
+   function timeToMSM(hour, min, mer) {
+       if (mer == "PM") 
+           hour += 12
+       return hour * 60 + min
+   }
+   
+   times = {
+       "t_us" : timeToMSM(11, 0, "PM"),
+       "t_ut" : timeToMSM(7, 0, "AM"),
+       "t_rs" : timeToMSM(11, 0, "PM"),
+       "t_rt" : timeToMSM(7, 0, "AM")
+   }
+   
+   t_us.timepicker({
+       defaultTime: "11:00 PM"
+   }).on('changeTime.timepicker', function(e) {
+       times.t_us = timeToMSM(e.time.hours, e.time.minutes, e.time.meridian);
+   });
+   t_ut.timepicker({
+       defaultTime: "7:00 AM"
+   }).on('changeTime.timepicker', function(e) {
+       times.t_ut = timeToMSM(e.time.hours, e.time.minutes, e.time.meridian);
+   });
+   t_rs.timepicker({
+       defaultTime: "11:00 PM"
+   }).on('changeTime.timepicker', function(e) {
+       times.t_rs = timeToMSM(e.time.hours, e.time.minutes, e.time.meridian);
+   });
+   t_rt.timepicker({
+       defaultTime: "7:00 AM"
+   }).on('changeTime.timepicker', function(e) {
+       times.t_rt = timeToMSM(e.time.hours, e.time.minutes, e.time.meridian);
+   });
+   
+   $("#submit").click(function(){
+       console.log(times);
+   });
 })
