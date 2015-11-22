08fe6abf78bb5e9615c9230ff34adecc58b8656e
Jacky Liang <jackyliang@berkeley.edu>
2015-11-21 21:24:54 -0800

    wip chart

--- js/index.js
+++ js/index.js
@@ -69,13 +69,52 @@ $(document).ready(function(){
        durs.r = getDuration(times.rs, times.rt);
    }
    
+   var data = [
+  {"title":"Sleep Duration","subtitle":"Hours","ranges":[12],"measures":[8,8],"markers":[8]},
+  {"title":"Circadian Rhythm Match","subtitle":"%","ranges":[100],"measures":[80],"markers":[90]},
+  {"title":"Order Size","subtitle":"US$, average","ranges":[350,500,600],"measures":[100,320],"markers":[550]},
+  {"title":"New Customers","subtitle":"count","ranges":[1400,2000,2500],"measures":[1000,1650],"markers":[2100]},
+  {"title":"Satisfaction","subtitle":"out of 5","ranges":[3.5,4.25,5],"measures":[3.2,4.7],"markers":[4.4]}
+    ];
+   
+   var margin = {top: 5, right: 40, bottom: 20, left: 120},
+        width = 960 - margin.left - margin.right,
+        height = 50 - margin.top - margin.bottom;
+
+    var chart = d3.bullet()
+        .width(width)
+        .height(height);
+
+    var svg = d3.select("#charts").selectAll("svg")
+        .data(data)
+        .enter().append("svg")
+        .attr("class", "bullet")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var title = svg.append("g")
+        .style("text-anchor", "end")
+        .attr("transform", "translate(-6," + height / 2 + ")");
+
+    title.append("text")
+        .attr("class", "title")
+        .text(function(d) { return d.title; });
+
+    title.append("text")
+        .attr("class", "subtitle")
+        .attr("dy", "1em")
+        .text(function(d) { return d.subtitle; });   
+   
    $("#submit").click(function(){
        //convert input times to calculable values 
        updateState();
        
        //plot stats
-       
+       svg.call(chart.duration(1000)); 
        
        //display warnings
    });
 })
+
