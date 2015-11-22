c276e8c0c7db23f42d66d82e23da594f90eaecc4
Jacky Liang <jackyliang@berkeley.edu>
2015-11-21 23:17:02 -0800

    roughly finished responsive d3 bullet chart

--- js/index.js
+++ js/index.js
@@ -8,11 +8,35 @@ $(document).ready(function(){
        lineColor: '#004471'
    }); 
    
+   var state = {
+       "submitted_once" : false
+   };
+   
    var constants = {
        "mins_in_day" : 24 * 60,
-       "mins_in_cycle" : 90
-   }
+       "mins_in_cycle" : 90,
+       "margins" : {
+            top: 5, 
+            right: 10, 
+            bottom: 20, 
+            left: 10
+        }
+   };
    
+   var chart_container_ref = $("#chart_sleep_duration");
+   
+   var charts_containers = [
+        "chart_sleep_duration",
+        "chart_circadian_rhythm"
+   ];
+   
+   var bullets = [];
+   
+  var charts_data = {
+      "chart_sleep_duration" : {"ranges":[12],"measures":[8, 8],"markers":[8]},
+      "chart_circadian_rhythm" : {"ranges":[100],"measures":[80, 80],"markers":[90]}
+  };
+      
    var t_us = $('#t_us'),
          t_ut = $('#t_ut'),
          t_rs = $('#t_rs'),
@@ -68,53 +92,63 @@ $(document).ready(function(){
        durs.u = getDuration(times.us, times.ut);
        durs.r = getDuration(times.rs, times.rt);
    }
+         
+   var width, height;
+   function updateChartDims() {
+       width = chart_container_ref.width() - constants.margins.left - constants.margins.right;
+       height = 50 - constants.margins.top - constants.margins.bottom;
+   };
+   updateChartDims();
    
-   var data = [
-  {"title":"Sleep Duration","subtitle":"Hours","ranges":[12],"measures":[8,8],"markers":[8]},
-  {"title":"Circadian Rhythm Match","subtitle":"%","ranges":[100],"measures":[80],"markers":[90]},
-  {"title":"Order Size","subtitle":"US$, average","ranges":[350,500,600],"measures":[100,320],"markers":[550]},
-  {"title":"New Customers","subtitle":"count","ranges":[1400,2000,2500],"measures":[1000,1650],"markers":[2100]},
-  {"title":"Satisfaction","subtitle":"out of 5","ranges":[3.5,4.25,5],"measures":[3.2,4.7],"markers":[4.4]}
-    ];
+   function initCharts() {
+       charts_containers.forEach(function(chart_container){
+           console.log(chart_container);
+            var chart = d3.bullet();
+            var svg = d3.select("#" + chart_container).selectAll("svg")
+                                .data([charts_data[chart_container]])
+                                .enter().append("svg")
+                                .attr("class", "bullet")
+                                .attr("width", width + constants.margins.left + constants.margins.right)
+                                .attr("height", height + constants.margins.top + constants.margins.bottom)
+                                .append("g")
+                                .attr("transform", "translate(10,0)");
+            chart.width(width).height(height);
+            bullets.push({
+                "chart" : chart,
+                "svg" : svg
+            })
+       });
+   };
+      
+   function updateChartData() {
+       
+   };
    
-   var margin = {top: 5, right: 40, bottom: 20, left: 120},
-        width = 960 - margin.left - margin.right,
-        height = 50 - margin.top - margin.bottom;
-
-    var chart = d3.bullet()
-        .width(width)
-        .height(height);
-
-    var svg = d3.select("#charts").selectAll("svg")
-        .data(data)
-        .enter().append("svg")
-        .attr("class", "bullet")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var title = svg.append("g")
-        .style("text-anchor", "end")
-        .attr("transform", "translate(-6," + height / 2 + ")");
-
-    title.append("text")
-        .attr("class", "title")
-        .text(function(d) { return d.title; });
-
-    title.append("text")
-        .attr("class", "subtitle")
-        .attr("dy", "1em")
-        .text(function(d) { return d.subtitle; });   
+   d3.select(window).on('resize', function(){
+       updateChartDims();
+       d3.selectAll("svg").attr("width", width + constants.margins.left + constants.margins.right);
+       bullets.forEach(function(bullet){
+            bullet.chart.width(width).height(height);
+            if (state.submitted_once)
+                bullet.svg.call(bullet.chart);        
+       });
+   });
    
    $("#submit").click(function(){
+       state.submitted_once = true;
        //convert input times to calculable values 
-       updateState();
-       
+        updateState();
+              
        //plot stats
-       svg.call(chart.duration(1000)); 
+        updateChartData();
+        bullets.forEach(function(bullet){
+            bullet.svg.call(bullet.chart.duration(1000));     
+       });
+
        
        //display warnings
    });
+   
+  initCharts();
 })
 
